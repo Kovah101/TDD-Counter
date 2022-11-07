8ce627271053a0d7aaecdfb9301d92284ea7f0ba
Elliot Thompson <elliotthompson101@gmail.com>
2022-11-07 10:34:25 +0000

    still trying to get database testing to run

--- app/src/androidTest/java/com/example/tddcounter/CountDatabaseTest.kt
+++ app/src/androidTest/java/com/example/tddcounter/CountDatabaseTest.kt
@@ -1,58 +1,43 @@
-package com.example.tddcounter.test
+package com.example.tddcounter
 
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.tddcounter.database.Count
 import com.example.tddcounter.database.CountDAO
 import com.example.tddcounter.database.CountDatabase
 import kotlinx.coroutines.flow.count
+import org.junit.Assert.*
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
-class CounterDatabaseTest {
+class CountDatabaseTest {
     private lateinit var countDAO: CountDAO
     private lateinit var db: CountDatabase
 
     @Before
-    fun createDb() {
+    fun setUp() {
         val context = ApplicationProvider.getApplicationContext<Context>()
-        db = Room.inMemoryDatabaseBuilder(context, CountDatabase::class.java).build()
+        db = Room.inMemoryDatabaseBuilder(
+            context, CountDatabase::class.java
+        ).build()
         countDAO = db.countDao()
     }
 
     @After
-    @Throws(IOException::class)
-    fun closeDb(){
+    fun tearDown() {
         db.close()
     }
 
     @Test
     @Throws(Exception::class)
-    suspend fun initialiseCountTest(){
+    suspend fun initialiseCountTest() {
+        //runCatching {
         countDAO.startNewCount()
         assertEquals(countDAO.getCount().count(), 0)
+        //}
     }
-
-    @Test
-    @Throws(Exception::class)
-    suspend fun countDaoFunctionsTest(){
-        countDAO.startNewCount()
-
-        countDAO.updateCount(Count(count = 4))
-        assertEquals(countDAO.getCount().count(), 4)
-
-        countDAO.clearCount()
-        assertEquals(countDAO.tableCount(), 0)
-    }
-
-
-
-
 }
--- app/src/main/java/com/example/tddcounter/database/CountDatabase.kt
+++ app/src/main/java/com/example/tddcounter/database/CountDatabase.kt
@@ -1,9 +1,12 @@
 package com.example.tddcounter.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Count::class], version = 1)
 abstract class CountDatabase : RoomDatabase() {
     abstract fun countDao(): CountDAO
+
 }
